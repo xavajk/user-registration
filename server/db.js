e4aa48723306e93da45d23f9588cda2b46483587
xavajk <xjkuehn@gmail.com>
2023-02-04 15:33:59 -0800

    backend set up with routes and user auth

---
+++ server/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+module.exports = () => {
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  try {
+    mongoose.set("strictQuery", true);
+    mongoose.connect(process.env.DB);
+    console.log("Connected to database successfully");
+  } catch (error) {
+    console.log(error);
+    console.log("Could not connect to database");
+  }
+};
---
+++ server/routes/auth.js
@@ -0,0 +1,37 @@
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const joi = require("joi");
+
+const { User } = require("../models/user");
+
+router.post("/", async (req, res) => {
+  try {
+    const { error } = validate(req.body);
+    if (error)
+      return res.status(400).send({ message: error.details[0].message });
+
+    const user = await User.findOne({ email: req.body.email });
+    if (!user)
+      return res.status(401).send({ message: "Invalid email or password" });
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword)
+      return res.status(401).send({ message: "Invalid email or password" });
+
+    const token = user.generateAuthToken();
+    res.status(200).send({ data: token, message: "Logged in successfully" });
+  } catch (error) {
+    res.status(500).send({ message: "Internal server error" });
+  }
+});
+
+const validate = (data) => {
+  const schema = joi.object({
+    email: joi.string().email().required().label("Email"),
+    password: joi.string().required().label("Password"),
+  });
+  return schema.validate(data);
+};
